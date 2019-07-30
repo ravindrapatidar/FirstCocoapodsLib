72f71e17590ec6e97effdf7080179872711f7edf
ravindrapatidar <ravindra.patidar@talentica.com>
2019-07-30 21:48:51 +0530

    initial Commit

---
+++ FirstCocoapodsLib/FirstCocoapodsLib/FirstCocoapodsLib.h
@@ -0,0 +1,19 @@
+//
+//  FirstCocoapodsLib.h
+//  FirstCocoapodsLib
+//
+//  Created by Ravindra Patidar on 30/07/19.
+//  Copyright © 2019 Ravindra Patidar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirstCocoapodsLib.
+FOUNDATION_EXPORT double FirstCocoapodsLibVersionNumber;
+
+//! Project version string for FirstCocoapodsLib.
+FOUNDATION_EXPORT const unsigned char FirstCocoapodsLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstCocoapodsLib/PublicHeader.h>
+
+
---
+++ FirstCocoapodsLib/FirstCocoapodsLibExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstCocoapodsLibExample
+//
+//  Created by Ravindra Patidar on 30/07/19.
+//  Copyright © 2019 Ravindra Patidar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
